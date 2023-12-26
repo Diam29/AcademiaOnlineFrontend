50e91fd15557804bbd180d718577575c4e49db8f
Diamela <diamelavlioi@gmail.com>
2023-12-26 14:01:23 -0300

    vite-react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import million from 'million/compiler';
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
 // import react from '@vitejs/plugin-react-swc'
 // import ViteSSR from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true })],
+  plugins: [million.vite({ auto: true }), react(),],
   // esbuild: {
   //   jsxFactory: 'React.createElement',
   //   jsxFragment: 'React.Fragment',
